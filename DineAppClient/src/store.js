076cd74990f1db1ad7b7477d6c1cc5f0c8641c12
gajananpojage <pojage@gmail.com>
2020-03-09 11:08:02 +0100

    Update store.js

--- DineAppClient/src/store.js
+++ DineAppClient/src/store.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-//import * as types from './mutation-types'
+import * as types from './mutation-types'
+import jobs from './jobsList.json'
 //import PostService from '@/services/PostService'
 
 Vue.use(Vuex)
@@ -8,21 +9,40 @@ const debug = process.env.NODE_ENV !== 'production'
 
 // initial state
 const state = {
-
+  jobId: 1,
+  allJobs: [],
 }
 // getters
 const getters = {
-
+  allJobs: state => state.allJobs,
 }
 // actions
 const actions = {
-
+  addJob({ commit }, job) {
+    commit(types.ADD_JOB, {
+      job
+    })   
+  },
+  incrementJobId (context) {
+    context.commit('incrementJobId')
+  },
+  setJobs(context){
+    context.commit('setJobs')
+  }
 }
 
 // mutations
 const mutations = {
-
-}
+  [types.ADD_JOB](state, { job }) {
+      state.allJobs.push(job)
+    },
+    incrementJobId(state) {
+      state.jobId++
+    },
+    setJobs(state) {
+      state.allJobs=jobs;
+    }
+  }
 // one store for entire application
 export default new Vuex.Store({
   state,
