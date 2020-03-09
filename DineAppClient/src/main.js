bba390727bd9f72f7adf10a79b6f123ef56e2cee
gajananpojage <pojage@gmail.com>
2020-03-09 11:09:39 +0100

    Update main.js

--- DineAppClient/src/main.js
+++ DineAppClient/src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
-import VueSweetalert2 from 'vue-sweetalert2'
-
-Vue.use(VueSweetalert2)
+import VueSweetalert2 from 'vue-sweetalert2';
+import DatetimePicker from 'vuetify-datetime-picker';
+Vue.use(DatetimePicker);
+Vue.use(VueSweetalert2);
+//Vue.use(DatetimePicker);
 
 Vue.config.productionTip = false
 
