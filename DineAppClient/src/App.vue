8e5f1abc48117867dc8f4ee553bc876f31ed4246
gajananpojage <pojage@gmail.com>
2020-03-11 16:44:05 +0100

    Update App.vue

--- DineAppClient/src/App.vue
+++ DineAppClient/src/App.vue
@@ -1,21 +1,27 @@
 <template>
   <v-app>
-   <Navbar />
-    <v-content >
+    <Navbar />
+    <v-content>
       <v-container fluid>
-      <router-view></router-view>
+        <router-view></router-view>
       </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import Navbar from './components/Navbar'
+import Navbar from "./components/Navbar";
 export default {
-  name: 'App',
+  name: "App",
   components: { Navbar },
   data: () => ({
     //
   }),
+  created() {
+    /* Set Initial Some Default Recordsfrom JSON for display in List*/
+    this.$store.dispatch("setJobs");
+    this.$store.dispatch("setValidations");
+    this.$store.dispatch("setProfiles");
+  }
 };
 </script>
