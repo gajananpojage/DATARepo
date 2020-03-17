5c1cc13029e037d279124044712dae47964df4e7
gajananpojage <pojage@gmail.com>
2020-03-17 14:10:00 +0100

    Update App.vue

--- DineAppClient/src/App.vue
+++ DineAppClient/src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-app>
-    <Navbar />
+  <v-app class="my-app">
+    <Navbar/>
     <v-content>
       <v-container fluid>
         <router-view></router-view>
@@ -11,17 +11,51 @@
 
 <script>
 import Navbar from "./components/Navbar";
+import { bus } from './main'
 export default {
   name: "App",
-  components: { Navbar },
+  components: { Navbar, },
   data: () => ({
-    //
+    loggedIn: false
   }),
+  props: {
+    //loggedIn: localStorage.getItem('loggedIn')
+  },
   created() {
+    bus.$on('loggedIn', (data) => {
+      this.loggedIn = data;
+    })
     /* Set Initial Some Default Recordsfrom JSON for display in List*/
     this.$store.dispatch("setJobs");
     this.$store.dispatch("setValidations");
     this.$store.dispatch("setProfiles");
+  },
+  mounted() {
+    // eslint-disable-next-line no-unused-vars
+    window.onpopstate = event => {
+      console.log(localStorage.getItem('loggedUser'));
+      if (localStorage.getItem('loggedIn') !== null && localStorage.getItem('loggedUser') === 'admin') {
+        this.$router.push('/onboarding') // redirect to home
+      } else if (localStorage.getItem('loggedIn') !== null && localStorage.getItem('loggedUser') !== 'admin') {
+        this.$router.push('/approval');
+      } else {
+        this.$router.push('/');
+      }
+    };
   }
 };
 </script>
+
+<style lang="scss">
+  .my-app.v-application .primary--text {
+    color: #00857a !important;
+    caret-color: #00857a !important;
+  }
+  .bg-color {
+    background-color:#00857a !important;
+  }
+
+  .text-white {
+    color: #fff !important;
+  }
+</style>
