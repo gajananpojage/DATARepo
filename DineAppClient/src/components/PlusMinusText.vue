84e968cbeaf62e824a76ee008d2ef188726992b5
gajananpojage <pojage@gmail.com>
2020-03-18 19:24:03 +0100

    Create PlusMinusText.vue

---
+++ DineAppClient/src/components/PlusMinusText.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+       <v-text-field 
+                v-model="plusminusCalc"
+                type="number"
+                label="Every"
+                append-outer-icon="add"
+                @click:append-outer="increment"
+                prepend-icon="remove"
+                @click:prepend="decrement"
+              ></v-text-field>
+
+</div>
+</template>
+<script>
+export default {
+  data: () => ({
+   plusminusCalc:0,
+  }),
+
+  methods: {
+   increment() {
+      this.plusminusCalc = parseInt(this.plusminusCalc, 10) + 1;
+      this.$emit("plusMinusValueChanged", this.plusminusCalc);
+    },
+    decrement() {
+      this.plusminusCalc = parseInt(this.plusminusCalc, 10) - 1;
+      this.$emit("plusMinusValueChanged", this.plusminusCalc);
+    },
+  }
+};
+</script>
