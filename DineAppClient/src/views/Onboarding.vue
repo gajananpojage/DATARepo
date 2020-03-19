74d3394e25c1eff42d4b435be921c4a662eb27c8
gajananpojage <pojage@gmail.com>
2020-03-19 21:45:09 +0100

    Update Onboarding.vue

--- DineAppClient/src/views/Onboarding.vue
+++ DineAppClient/src/views/Onboarding.vue
@@ -1,132 +1,32 @@
 <template>
-  <div class="dashboard">
-    <h3 class="subheading grey--text">Application OnBoarding</h3>
-    <v-container class="my-5">
-      <v-form class="px-3" ref="form" v-model="valid">    
-        <v-row>
-          <v-col cols="10">
-            <v-text-field
-                    v-model="applicationName"
-                    label="Application Name: "
-                    :rules="nameRules"
-                    :counter="20"
-                    required
-            ></v-text-field>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col cols="10">
-            <v-text-field
-                    v-model="applicationDescription"
-                    label="Description: "
-                    :rules="descriptionRules"
-                    :counter="100"
-                    required
-            ></v-text-field>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col cols="5">
-            <v-text-field
-                    v-model="applicationOwnerName"
-                    label="Owner Name: "
-                    :rules="nameRules"
-                    :counter="20"
-                    required
-            ></v-text-field>
-          </v-col>
-          <v-col cols="5">
-            <v-text-field
-                    v-model="applicationOwnerEmailId"
-                    label="Owner Email Id: "
-                    :rules="emailRules"
-                    required
-            ></v-text-field>
-            </v-col>
-        </v-row>
-        <v-row>
-          <v-col cols="10">
-            <v-select
-                    :items="sendNotification"
-                    v-model="toolName"
-                    label="Send Notification:"
-                    required
-                    :rules="valueRequiredRules"
-            ></v-select>
-          </v-col>
-        </v-row>
+  <div>
+    <h3 class="subheading grey--text">Application Onboarding</h3>
+    <v-tabs v-model="activeTab">
+      <v-tab v-for="tab of tabs" :key="tab.id" :to="tab.route" exact>
+        {{ tab.name }}
+      </v-tab>
 
-        <v-row>
-          <v-col cols="1">
-            <v-btn text @click="submit" class="mx-0 mt-3 bg-color text-white"
-            >Submit</v-btn
-            ><v-snackbar v-model="snackbar">
-            {{ submitSuccessfully }}
-            <v-btn color="green" text @click="snackbar = false">
-              Close
-            </v-btn>
-          </v-snackbar></v-col></v-row
-        >
-      </v-form>
-    </v-container>
+      <v-tab-item v-for="tab of tabs" :key="tab.id" :value="tab.route">
+        <router-view></router-view>
+      </v-tab-item>
+    </v-tabs>
   </div>
 </template>
 <script>
 export default {
-  data: () => ({
-    source: ["RDS", "OPSI", "GOLD", "DIAL"],
-    sendNotification: ["Service Now", "Email"],
-    applicationName: '',
-    applicationOwnerName: '',
-    applicationDescription: '',
-    applicationOwnerEmailId: '',
-    toolName: '',
-    submitSuccessfully: 'Data has been submitted successfully',
-    snackbar: false,
-    valid: false,
-    formObj: {},
-    nameRules: [
-      (v) => !!v || 'Name is required',
-      (v) => v && v.length <= 20 || 'Application name must be less than 20 characters'
-    ],
-    descriptionRules: [
-      (v) => !!v || 'Description is required',
-      (v) => v && v.length <= 100 || 'Description name must be less than 100 characters'
-    ],
-    valueRequiredRules: [
-      (v) => !!v || 'Value is required',
-    ],
-    email: '',
-    emailRules: [
-      v => !!v || 'E-mail is required',
-      v => /.+@.+/.test(v) || 'E-mail must be valid',
-    ],
-  }),
-
-  computed: {},
-  created() {},
-  methods: {
-    submit() {
-      if (this.$refs.form.validate()) {
-        this.snackbar = true;
-        this.formObj = {
-          'name': this.applicationName,
-          'ownerName':this.applicationOwnerName,
-          'emailId': this.applicationOwnerEmailId,
-          'description': this.applicationDescription,
-          'toolName': this.toolName
-        };
-        console.log(this.formObj);
-         this.$store.dispatch("addApplication", this.applicationName);
-        this.$router.push('/reconciliation');
-      }
-    }
+  name: "Onboarding",
+  data() {
+    return {
+      activeTab: `/onboarding`,
+      tabs: [
+        { id: 1, name: "Onboard", route: `/onboarding` },
+        { id: 2, name: "List", route: `/onboarding/list` }
+      ]
+    };
   }
-};
+}
 </script>
 
 <style>
-  .row {
-    margin-top: -10px;
-  }
+
 </style>
